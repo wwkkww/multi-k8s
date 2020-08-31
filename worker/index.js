6317364ea44a07c49f10f96053a71f4fdd1c2a4d
kevin <kevin@betacode.io>
2020-08-31 09:38:14 +0800

    setup worker process to calculate fib value

--- worker/index.js
+++ worker/index.js
@@ -1,1 +1,27 @@
 const keys = require('./keys')
+const redis = require('redis')
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  // attempt to reconnect every 1 second if redisClient lost connection with redis server
+  retry_strategy: () => 1000,
+})
+
+const sub = redisClient.duplicate()
+
+// fib sequence: 0,1,1,2,3,5,8,13,21,34,....
+// Recursive solution => (2^N) time complexity
+function fib(index) {
+  if (index < 2) return 1
+
+  return fib(index - 1) + fib(index - 2)
+}
+
+// run when new value receive by redis
+sub.on('message', (channel, message) => {
+  // hset('values', 6, 8)
+  redisClient.hset('values', message, fib(parseInt(message)))
+})
+
+sub.subscribe('insert')
